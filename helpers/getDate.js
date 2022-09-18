6b52d9e9a25c95253103a1ded0962946204ee48d
Whitney <designedbywh@gmail.com>
2022-09-18 14:10:23 -0400

    fix date for ios compatibility

--- helpers/getDate.js
+++ helpers/getDate.js
@@ -3,7 +3,8 @@ import { calculateDaysLeft, calculateFullDate } from "./calculateDate.js";
 export default function getDate(inputDate) {
   if (inputDate[0] === "") return { fullDate: null, daysLeft: null };
   
-  const newInputDate = `${inputDate[1]} ${inputDate[2]} ${inputDate[0]} 23:59:59`;
+  const newInputDate = `${inputDate[1]}/${inputDate[2]}/${inputDate[0]} 23:59:59`;
+
   const fullDate = calculateFullDate(newInputDate);
   const daysLeft = calculateDaysLeft(newInputDate);
 
