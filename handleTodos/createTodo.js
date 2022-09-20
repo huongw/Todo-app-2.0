109bd87c18d6699e2d01d0539fc49c81846d893e
Whitney <designedbywh@gmail.com>
2022-09-19 20:57:52 -0400

    change code for edge case

--- handleTodos/createTodo.js
+++ handleTodos/createTodo.js
@@ -74,7 +74,7 @@ export default function createTodo(data, todo, todoList, saveToLocalStorage) {
   
   saveButton.addEventListener("click", () => {
     const todoItem = document.querySelector(`#id${id} .todo`);
-    if (todoItem.innerHTML === "") return alert("Input field cannot be empty!");
+    if (!todoItem.textContent) return alert("Input field cannot be empty!");
 
     handleSaveCancelChanges(data, id, saveChanges, checkbox, saveButton, cancelButton, editButton)
     saveToLocalStorage(data)
